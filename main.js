bdf2ba91090ed9c39b7cf9a70b90368f09158461
N000173276 <benson.ye@npc.com.tw>
2022-05-16 17:28:06 +0800

    add pAequorFactory

--- main.js
+++ main.js
@@ -13,10 +13,32 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//Returns a pAequor object
+function pAequorFactory(specimenNum, dnaSequence) {
+  return {
+    specimenNum: specimenNum,
+    dna: dnaSequence,
+    mutate() {
+      //Choose one base and mutate to another base
+      const strandIndex = Math.floor(Math.random() * 15);
+      const dnaBases = ['A', 'T', 'C', 'G'];
+      const originalBase = this.dna[strandIndex];
+      
+      //kick original base out and randomly mutate
+      dnaBases.splice(dnaBases.indexOf(originalBase) ,1);
+      const mutateBase = dnaBases[Math.floor(Math.random() * 3)];
+      dnaSequence[strandIndex] = mutateBase;
+    }
+  };
+}
 
 
-
-
+const specimen1 = pAequorFactory(1, mockUpStrand());
+console.log(specimen1.dna);
+specimen1.mutate();
+console.log(specimen1.dna);
+specimen1.mutate();
+console.log(specimen1.dna);
 
 
 
